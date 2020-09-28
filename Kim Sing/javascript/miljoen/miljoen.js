e6afe37cf22513b860b62f3c9eb5fa1457267b84
Irene van Dijk <irene.a.vandijk@gmail.com>
2020-09-28 10:48:12 +0200

    JS test van Kris

---
+++ Kim Sing/javascript/miljoen/miljoen.js
@@ -0,0 +1,193 @@
+window.onbeforeunload = function myFunction() {
+    var txt;
+    if (confirm("Welkom bij Miljoenen Jacht!, druk op Ok om te beginnen")) {
+      txt = "Veel succes!";
+    } else {
+      txt = "Jammer!";
+    }
+    document.getElementById("demo").innerHTML = txt;
+  }
+  ​
+  ​
+  function my1() {
+    const geldbedrag = document.getElementById('k1');
+    const koffers = document.getElementById('b1');
+    geldbedrag.style.left='-200px'
+    geldbedrag.style.opacity='0';
+    koffers.style.opacity='0';
+    }
+    function my2() {
+      const geldbedrag = document.getElementById('k2');
+      const koffers = document.getElementById('b2');
+      geldbedrag.style.left='200px'
+      geldbedrag.style.opacity='0';
+      koffers.style.opacity='0';
+      }
+      function my3() {
+        const geldbedrag = document.getElementById('k3');
+        const koffers = document.getElementById('b3');
+        geldbedrag.style.left='-200px'
+        geldbedrag.style.opacity='0';
+        koffers.style.opacity='0';
+        }
+        function my4() {
+          const geldbedrag = document.getElementById('k4');
+          const koffers = document.getElementById('b4');
+          geldbedrag.style.left='200px'
+          geldbedrag.style.opacity='0';
+          koffers.style.opacity='0';
+          }
+          function my5() {
+            const geldbedrag = document.getElementById('k5');
+            const koffers = document.getElementById('b5');
+            geldbedrag.style.left='-200px'
+            geldbedrag.style.opacity='0';
+            koffers.style.opacity='0';
+            }
+            function my6() {
+              const geldbedrag = document.getElementById('k6');
+              const koffers = document.getElementById('b6');
+              geldbedrag.style.left='200px'
+              geldbedrag.style.opacity='0';
+              koffers.style.opacity='0';
+              }
+              function my7() {
+                const geldbedrag = document.getElementById('k7');
+                const koffers = document.getElementById('b7');
+                geldbedrag.style.left='-200px'
+                geldbedrag.style.opacity='0';
+                koffers.style.opacity='0';
+                }
+                function my8() {
+                  const geldbedrag = document.getElementById('k8');
+                  const koffers = document.getElementById('b8');
+                  geldbedrag.style.left='200px'
+                  geldbedrag.style.opacity='0';
+                  koffers.style.opacity='0';
+                  }
+                  function my9() {
+                    const geldbedrag = document.getElementById('k9');
+                    const koffers = document.getElementById('b9');
+                    geldbedrag.style.left='-200px'
+                    geldbedrag.style.opacity='0';
+                    koffers.style.opacity='0';
+                    }
+                    function my10() {
+                      const geldbedrag = document.getElementById('k10');
+                      const koffers = document.getElementById('b10');
+                      geldbedrag.style.left='200px'
+                      geldbedrag.style.opacity='0';
+                      koffers.style.opacity='0';
+                      }
+                      function my11() {
+                        const geldbedrag = document.getElementById('k11');
+                        const koffers = document.getElementById('b11');
+                        geldbedrag.style.left='-200px'
+                        geldbedrag.style.opacity='0';
+                        koffers.style.opacity='0';
+                        }
+                        function my12() {
+                          const geldbedrag = document.getElementById('k12');
+                          const koffers = document.getElementById('b12');
+                          geldbedrag.style.left='200px'
+                          geldbedrag.style.opacity='0';
+                          koffers.style.opacity='0';
+                          }
+                          function my13() {
+                            const geldbedrag = document.getElementById('k13');
+                            const koffers = document.getElementById('b13');
+                            geldbedrag.style.left='-200px'
+                            geldbedrag.style.opacity='0';
+                            koffers.style.opacity='0';
+                            }
+                            function my14() {
+                              const geldbedrag = document.getElementById('k14');
+                              const koffers = document.getElementById('b14');
+                              geldbedrag.style.left='200px'
+                              geldbedrag.style.opacity='0';
+                              koffers.style.opacity='0';
+                              }
+                              function my15() {
+                                const geldbedrag = document.getElementById('k15');
+                                const koffers = document.getElementById('b15');
+                                geldbedrag.style.left='-200px'
+                                geldbedrag.style.opacity='0';
+                                koffers.style.opacity='0';
+                                }
+                                function my16() {
+                                  const geldbedrag = document.getElementById('k16');
+                                  const koffers = document.getElementById('b16');
+                                  geldbedrag.style.left='200px'
+                                  geldbedrag.style.opacity='0';
+                                  koffers.style.opacity='0';
+                                  }
+                                  function my17() {
+                                    const geldbedrag = document.getElementById('k17');
+                                    const koffers = document.getElementById('b17');
+                                    geldbedrag.style.left='-200px'
+                                    geldbedrag.style.opacity='0';
+                                    koffers.style.opacity='0';
+                                    }
+                                    function my18() {
+                                      const geldbedrag = document.getElementById('k18');
+                                      const koffers = document.getElementById('b18');
+                                      geldbedrag.style.left='200px'
+                                      geldbedrag.style.opacity='0';
+                                      koffers.style.opacity='0';
+                                      }
+                                      function my19() {
+                                        const geldbedrag = document.getElementById('k19');
+                                        const koffers = document.getElementById('b19');
+                                        geldbedrag.style.left='-200px'
+                                        geldbedrag.style.opacity='0';
+                                        koffers.style.opacity='0';
+                                        }
+                                        function my20() {
+                                          const geldbedrag = document.getElementById('k20');
+                                          const koffers = document.getElementById('b20');
+                                          geldbedrag.style.left='200px'
+                                          geldbedrag.style.opacity='0';
+                                          koffers.style.opacity='0';
+                                          }
+                                          function my21() {
+                                            const geldbedrag = document.getElementById('k21');
+                                            const koffers = document.getElementById('b21');
+                                            geldbedrag.style.left='-200px'
+                                            geldbedrag.style.opacity='0';
+                                            koffers.style.opacity='0';
+                                            }
+                                            function my22() {
+                                              const geldbedrag = document.getElementById('k22');
+                                              const koffers = document.getElementById('b22');
+                                              geldbedrag.style.left='200px'
+                                              geldbedrag.style.opacity='0';
+                                              koffers.style.opacity='0';
+                                              }
+                                              function my23() {
+                                                const geldbedrag = document.getElementById('k23');
+                                                const koffers = document.getElementById('b23');
+                                                geldbedrag.style.left='-200px'
+                                                geldbedrag.style.opacity='0';
+                                                koffers.style.opacity='0';
+                                                }
+                                                function my24() {
+                                                  const geldbedrag = document.getElementById('k24');
+                                                  const koffers = document.getElementById('b24');
+                                                  geldbedrag.style.left='200px'
+                                                  geldbedrag.style.opacity='0';
+                                                  koffers.style.opacity='0';
+                                                  }
+                                                  function my25() {
+                                                    const geldbedrag = document.getElementById('k25');
+                                                    const koffers = document.getElementById('b25');
+                                                    geldbedrag.style.left='-200px'
+                                                    geldbedrag.style.opacity='0';
+                                                    koffers.style.opacity='0';
+                                                    }
+                                                    function my26() {
+                                                      const geldbedrag = document.getElementById('k26');
+                                                      const koffers = document.getElementById('b26');
+                                                      geldbedrag.style.left='200px'
+                                                      geldbedrag.style.opacity='0';
+                                                      koffers.style.opacity='0';
+                                                    }  
